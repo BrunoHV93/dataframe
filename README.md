5b1a8d2f31eebb3c949a1006f1d41cda931ae2bd
Bruno Hernández <brunohervi93@gmail.com>
2021-07-30 21:19:06 -0500

    se agrega lista y readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# dataframe
+## Dataframe
+
+**Descripción**
+
+Dado un conjunto de listas, se agrupan en una lista y esta se transforma en un data frame, posteriormente el dataframe se guarda en un archivo cvs.
+Para este programa se utiliza la librería pandas.
+
+Bruno GHV.
--- dataframe.py
+++ dataframe.py
@@ -1,7 +1,4 @@
-#Importación de pandas
-import pandas as pd
-import csv
-
+#Primer proyecto de uso de dataframes
 #Declaración de listas
 fbk=['Facebook', 2449, True, 2006]
 twt=['Twitter', 339, False, 2006]
@@ -9,9 +6,10 @@ ig=['Instagram', 1000, True, 2010]
 yt=['Youtube', 2000, False, 2005]
 lkn=['LinkedIn', 663, False, 2003]
 wsp=['WhatsApp', 1600, True, 2009]
+tktk=['Tiktok',2000, False, 2020]
 
 #Asignamos listas a una lista
-lista_rrss=[fbk,twt,ig,yt,lkn,wsp]
+lista_rrss=[fbk,twt,ig,yt,lkn,wsp,tktk]
 
 #Construcción de dataframe
 df_rrss=pd.DataFrame(
@@ -24,5 +22,6 @@ print(df_rrss)
 
 
 #Guardado como un CSV
-df_rrss.to_csv("c:/Users/monst/Desktop/Python/basicos/redes.csv")
+df_rrss.to_csv("c:/Users/monst/Desktop/Python/dataframe/redes.csv")
+ 
  
